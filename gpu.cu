31fc44fde1947c5811a53bdcbe91cc3cdcb9d9a4
Ed Younis <edyounis123@gmail.com>
2020-03-02 13:28:40 -0800

    Initial Commit

---
+++ gpu.cu
@@ -0,0 +1,16 @@
+#include "common.h"
+#include <cuda.h>
+
+// Put any static global variables here that you will use throughout the simulation.
+
+void init_simulation(particle_t* parts, int num_parts, double size) {
+    // You can use this space to initialize data objects that you may need
+    // This function will be called once before the algorithm begins
+    // parts live in GPU memory
+    // Do not do any particle simulation here
+}
+
+void simulate_one_step(particle_t* parts, int num_parts, double size) {
+    // parts live in GPU memory
+    // Write this function
+}
